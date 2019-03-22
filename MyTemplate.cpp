d780c97c31b2361acf79be69cc72d760b6aa21be
Deepanshu Yadav <daybond007@gmail.com>
2019-03-23 00:49:23 +0530

    MyTemplate

---
+++ MyTemplate.cpp
@@ -0,0 +1,23 @@
+/*
+    Name:Deepanshu Yadav
+    Handle:FireFrost/acevenom
+*/
+
+#include<bits/stdc++.h>
+
+#define ll long
+#define l(i,k,n) for(int i=k; i<n; i++ )
+#define dl(i,k,n) for(int i=k; i>n; i--)
+#define nl cout<<"\n"
+#define tcloop  long i=0;cin>>i;while(i--)
+int gcd(long a,long b){ return(b==0?a:gcd(b,a%b));}
+int lcm(long a,long b){ return((a*b)/gcd(a,b));}
+
+using namespace std;
+
+int main(){
+
+    //the real code
+
+return 0;
+}
